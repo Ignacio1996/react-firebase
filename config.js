983b068edca05e462cfa7bea67e8b92a536bb300
Ignacio1996 <in-aguirre@hotmail.com>
2019-04-04 15:04:56 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyBiWuNKIOVeEHzGjDYxvi09fMgnrNyofpA",
-    authDomain: "react-firebase-698db.firebaseapp.com",
-    databaseURL: "https://react-firebase-698db.firebaseio.com",
-    projectId: "react-firebase-698db",
-    storageBucket: "react-firebase-698db.appspot.com",
-    messagingSenderId: "947393276016"
-  };
